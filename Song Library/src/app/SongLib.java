ef5f13ab31db5abada91ea7d9099edc16fa3b4c4
pedro <pedro@10.0.0.99>
2022-02-07 12:51:19 -0500

    added Imports and basic info. No functionality yet

--- Song Library/src/app/SongLib.java
+++ Song Library/src/app/SongLib.java
@@ -1,19 +1,35 @@
 package app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import javafx.stage.Stage;
 
 public class SongLib extends Application{
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		// create FXML loader
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(getClass().getResource("/views/root.fxml"));
+				
+				// load fmxl, root layout manager in fxml file is GridPane
+				GridPane root = (GridPane)loader.load();
+
+				// set scene to root
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.setResizable(false);
+				primaryStage.show();
 		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		launch(args);
 	}
 
 
